9eda5a43c4b1f2fff6504aa4874ef9319c041c63
MateHUNAa <you@example.com>
2026-02-26 11:31:34 +0100

    Update snippet options in EditSnippets method
    Added "oxLib" and "lsCore" snippet options with empty tags. Removed "Repository", "DomainEntity", and "InstanceWrapper" options, which previously had the "ls" tag. Other snippet options remain unchanged.

--- App/Commands/EditCommand.cs
+++ App/Commands/EditCommand.cs
@@ -124,15 +124,14 @@ namespace fvm.Commands
         {
             var snippetOptions = new List<SnippetOption>
             {
+                new() { Name = "oxLib",  Tags = { } },
+                new() { Name = "lsCore",  Tags = { } },
                 new() { Name = "Grid-system",   Tags = { } },
                 new() { Name = "Logger-system", Tags = { } },
                 new() { Name = "OxMySQL",       Tags = { } },
                 new() { Name = "ESX",           Tags = { "esx" } },
                 new() { Name = "NUI",           Tags = { "react" } },
                 new() { Name = "ClientLoader",  Tags = { } },
-                new() { Name = "Repository",    Tags = { "ls" } },
-                new() { Name = "DomainEntity",  Tags = { "ls" } },
-                new() { Name = "InstanceWrapper", Tags = { "ls" } },
                 new() { Name = "EventHandler",  Tags = { } },
             };
 
